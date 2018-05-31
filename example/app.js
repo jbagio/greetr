ecfcbbbfc200f48107287271f7abacc7b0fcc176
João Bagio <34991522+jbagio@users.noreply.github.com>
2018-05-31 13:23:52 +0100

    Update example usage page

--- example/app.js
+++ example/app.js
@@ -1,10 +1,31 @@
-const g = G$('John', 'Doe');
+// const g = G$('Jane', 'Doe');
+//
+// // informal greeting
+// g.greet();
+//
+// // chained with formal greeting
+// g.greet().greet(true);
+//
+// // chained with setLanguage to Spanish
+// g.greet().setLanguage('es').greet(true);
 
-// informal greeting
-g.greet();
+const loginDiv = document.getElementById('login-div');
+const loginBtn = document.getElementById('login');
+const h1 = document.getElementById('greeting');
+const select = document.getElementById('lang');
 
-// chained with formal greeting
-g.greet().greet(true);
+// Login btn click event
+loginBtn.addEventListener('click', (e) => {
 
-// chained with setLanguage to hipster
-g.setLanguage('hip').greet();
+  // Get a new Greetr object
+  // notice the 'new' keyword is not needed here
+  // for the sake of simplicity the name is hardcoded here
+  const g = G$('Jane', 'Doe');
+
+  // Use our chainable methods to set the language,
+  // fire off an HTML greeting passing the h1 as the selector and log to the console
+  g.setLanguage(select.value).HTMLGreeting(h1, true).log();
+
+  // Hide the login section
+  loginDiv.style.display = 'none';
+});
